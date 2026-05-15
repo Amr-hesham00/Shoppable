83c365d8fee07ea7eaff86cf5454d5fb5812350a
amr heshamm <amrheshamf321@gmail.com>
2026-05-15 22:44:28 +0300

    123

--- obj/Debug/net9.0/Shoppable.AssemblyInfo.cs
+++ obj/Debug/net9.0/Shoppable.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shoppable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f78c9c57c96dd01e9b342c3b5bf1653e958fccc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shoppable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shoppable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
